884dc33afa12395555c7a217040c75f7873f5785
RaphMt <rafa.mtorres117@outlook.com>
2023-08-31 20:33:53 -0300

    lesson 8 finished

--- intro-to-vue/Intro-to-Vue-3-L8-start/main.js
+++ intro-to-vue/Intro-to-Vue-3-L8-start/main.js
@@ -1,24 +1,36 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
+            selectedVariant: 0,
             image: './assets/images/socks_blue.jpg',
             inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+            ],
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.image = index
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
         }
-    }
+    },
 })
